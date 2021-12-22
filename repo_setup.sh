1fb9ba1fe0956cf153a1e6addb7b8ee689167e31
DragonCrafted87 <24697493+DragonCrafted87@users.noreply.github.com>
2021-12-22 12:08:39 -0600

    just some cleanup

--- repo_setup.sh
+++ repo_setup.sh
@@ -19,31 +19,31 @@ case "$OSTYPE" in
             mingw-w64-clang-x86_64-qt6-debug \
             mingw-w64-clang-x86_64-qt6-static \
             mingw-w64-clang-x86_64-toolchain
+
+        pip install lizard
+        pip install pre-commit
+
+        pushd /tmp
+        curl -L https://api.github.com/repos/include-what-you-use/include-what-you-use/tarball/refs/tags/0.17 --output iwyu.tar.gz
+        mkdir iwyu
+        bsdtar -xvf iwyu.tar.gz -C iwyu --strip-components 1
+        rm iwyu.tar.gz
+        pushd iwyu
+        mkdir build
+        pushd build
+
+        cmake  \
+            -S .. \
+            -GNinja \
+            -Wno-dev \
+            -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-clang++ \
+            -DCMAKE_C_COMPILER=x86_64-w64-mingw32-clang \
+            -DCMAKE_INSTALL_PREFIX=/clang64/
+
+        cmake --build . --parallel "$(nproc)"
+        cmake --install .
         ;;
     *)
         return
         ;;
 esac
-
-pip install lizard
-pip install pre-commit
-
-pushd /tmp
-curl -L https://api.github.com/repos/include-what-you-use/include-what-you-use/tarball/refs/tags/0.17 --output iwyu.tar.gz
-mkdir iwyu
-bsdtar -xvf iwyu.tar.gz -C iwyu --strip-components 1
-rm iwyu.tar.gz
-pushd iwyu
-mkdir build
-pushd build
-
-cmake  \
-    -S .. \
-    -GNinja \
-    -Wno-dev \
-    -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-clang++ \
-    -DCMAKE_C_COMPILER=x86_64-w64-mingw32-clang \
-    -DCMAKE_INSTALL_PREFIX=/clang64/
-
-cmake --build . --parallel "$(nproc)"
-cmake --install .
